e6aa5c67c50c4939f6db3c8688fe12744096b2fc
louisrosenblum <42879481+louisrosenblum@users.noreply.github.com>
2020-01-28 14:23:22 -0700

    Implemented VHDL 2008 libraries

--- HW #4/newton_block.vhd
+++ HW #4/newton_block.vhd
@@ -2,14 +2,17 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 USE ieee.numeric_std.ALL;
 
+library IEEE_proposed;
+use IEEE_proposed.fixed_pkg.all;
+
 entity newton_block is
 
 	generic (w_bits : positive := 32; -- size of word
 		 F_bits : positive := 16); -- number of fractional bits
 
-	port(input_x : in unsigned(w_bits-1 downto 0);
-		input_y : in unsigned(w_bits-1 downto 0);
-		output_y : out unsigned(w_bits-1 downto 0));
+	port(input_x : in ufixed(w_bits-F_bits-1 downto -F_bits);
+		input_y : in ufixed(w_bits-F_bits-1 downto -F_bits);
+		output_y : out ufixed(w_bits-F_bits-1 downto -F_bits));
 	
 
 end entity;
@@ -21,18 +24,12 @@ begin
 
 main: process(input_x,input_y)
 
-	variable x_in : integer;
-	variable y_in : integer;
-	variable y_out : integer;
+variable yn : ufixed(w_bits-F_bits-1 downto -F_bits);
 
 begin
 
-	x_in := to_integer(input_x(w_bits-1 downto F_bits)) + to_integer(input_x(F_bits-1 downto 0))/(2**F_bits);
-	y_in := to_integer(input_y(w_bits-1 downto F_bits)) + to_integer(input_y(F_bits-1 downto 0))/(2**F_bits);
-	
-	y_out := y_in * (3-(x_in * (y_in)**2))/2;
-
-	output_y(w_bits-1 downto 0) <= to_unsigned(y_out,w_bits);
+	yn := input_y*(3 - (input_x*(input_y*input_y)))/2;
+	output_y <= yn;
 
 end process;
 
